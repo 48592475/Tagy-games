f04658e435a27e434efb7f7cdffc3b31e4b96e24
gabofrid <143649861+gabofrid@users.noreply.github.com>
2024-08-08 14:31:29 -0300

    Agregue que se puedan sacar fotos

--- IA-Emociones/main.py
+++ IA-Emociones/main.py
@@ -1,16 +1,45 @@
 #importanciones
 import cv2
 import numpy as np
+import os
+import glob
+#from deepface import DeepFace
 
+carpeta = "Fotos.Emociones"
 #veo que version son
 print("OpenCV version:", cv2.__version__)
 print("NumPy version:", np.__version__)
 
 #abro la cámara
+
 cap = cv2.VideoCapture(1)
 if not cap.isOpened():
     print("Error: No se puede abrir la cámara")
     exit()
+
+#def AnalizarFotos ():
+   # UltimaFoto = obtener_ultimo_jpg(carpeta)
+   # analisis = DeepFace.analyze(foto , actions=["emotion"])
+    
+def obtener_ultimo_jpg(carpeta):
+    archivos_jpg = [f for f in os.listdir(carpeta) if f.endswith('.jpg')]
+    
+    if not archivos_jpg:
+        return None  # No hay archivos JPG en la carpeta
+
+    archivos_jpg.sort(key=lambda f: os.path.getctime(os.path.join(carpeta, f)))
+    ultimo_jpg = archivos_jpg[-1]
+    
+    return os.path.join(carpeta, ultimo_jpg)
+
+def TomarFoto (carpeta, nombre):
+    for (x, y, w, h) in faces:
+        roi = frame[y:y+h, x:x+w]
+        ubicacion = os.path.join(carpeta, nombre)
+        cv2.imwrite(ubicacion, roi)
+        print ("Se guardo en la carpeta" , carpeta , "con el nombre" , nombre)
+        count =+ 1
+        ultima_foto = os.path.join( "Fotos.Emociones", f"foto_{count}.jpg")
 
 #importo el reconocedor de caras
 cascade_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
@@ -31,7 +60,7 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     for (x, y, w, h) in faces:    #hago un rectangulo que se ponga encima de las caras detectadas
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 150), 3)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
 
@@ -40,5 +69,10 @@ while True:
     if cv2.waitKey(1) == ord('q'):         #para cerrar la camara tocar la letra q
         break
 
+    if cv2.waitKey(1) == ord('s'): # para tomar foto letra s
+        ListaDeFotos = glob.glob(os.path.join("Fotos.Emociones", '*'))
+        count = len(ListaDeFotos) + 1
+        TomarFoto("Fotos.Emociones", f'Foto nro{count}.jpg')
+    
 cap.release()
 cv2.destroyAllWindows()
