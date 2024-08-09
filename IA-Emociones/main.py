837550049b32a997064d804eb7b64001baa9c011
gabofrid <143649861+gabofrid@users.noreply.github.com>
2024-08-09 10:30:26 -0300

    automatización de fotos cada 3 segundos

--- IA-Emociones/main.py
+++ IA-Emociones/main.py
@@ -3,9 +3,12 @@ import cv2
 import numpy as np
 import os
 import glob
+import time
 #from deepface import DeepFace
 
 carpeta = "Fotos.Emociones"
+count = 0
+
 #veo que version son
 print("OpenCV version:", cv2.__version__)
 print("NumPy version:", np.__version__)
@@ -18,8 +21,9 @@ if not cap.isOpened():
     exit()
 
 #def AnalizarFotos ():
-   # UltimaFoto = obtener_ultimo_jpg(carpeta)
-   # analisis = DeepFace.analyze(foto , actions=["emotion"])
+ # foto = 'C:\Users\48113111\Documents\Juego 2\proyecto\Tagy-games\IA-Emociones\Fotos.Emociones'
+  #analisis = DeepFace.analyze(foto , actions=["emotion"])
+ #print (analisis)
     
 def obtener_ultimo_jpg(carpeta):
     archivos_jpg = [f for f in os.listdir(carpeta) if f.endswith('.jpg')]
@@ -40,10 +44,17 @@ def TomarFoto (carpeta, nombre):
         print ("Se guardo en la carpeta" , carpeta , "con el nombre" , nombre)
         count =+ 1
         ultima_foto = os.path.join( "Fotos.Emociones", f"foto_{count}.jpg")
+        return(ultima_foto)
+
+
 
 #importo el reconocedor de caras
 cascade_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(cascade_path)
+
+
+# Bucle principal
+inicio = time.time()
 
 if face_cascade.empty():
     print("Error: No se puede cargar el clasificador de cascada")
@@ -60,12 +71,21 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     for (x, y, w, h) in faces:    #hago un rectangulo que se ponga encima de las caras detectadas
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 150), 3)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (103, 46, 1), 3)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
 
-    cv2.imshow('frame', frame)
+  # Verifica si han pasado 3 segundos
+    tiempo_actual = time.time()
+    tiempo_transcurrido = tiempo_actual - inicio
 
+    if int(tiempo_transcurrido) % 3 == 0:  # Si el tiempo transcurrido es divisible por 3
+        nombre = f'Foto_auto_{count + 1}.jpg'
+        TomarFoto(carpeta, nombre)
+        time.sleep(1)
+       # AnalizarFotos() 
+      
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):         #para cerrar la camara tocar la letra q
         break
 
