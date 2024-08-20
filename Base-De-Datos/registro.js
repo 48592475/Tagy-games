698492abb6dbbfceecb74aa800c13109ac5334e7
48592475 <144272436+48592475@users.noreply.github.com>
2024-08-20 10:47:56 -0300

    Registrarse-Postam-Funcionando

--- Base-De-Datos/registro.js
+++ Base-De-Datos/registro.js
@@ -3,7 +3,8 @@ const { Pool } = require('pg');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3003;
+const port = 3012;  
+
 const pool = new Pool({
     user: 'default',       
     host: 'ep-restless-breeze-a4rx7skq.us-east-1.aws.neon.tech',
@@ -20,18 +21,26 @@ app.post('/registrar', async (req, res) => {
     const { usuario, nombre, apellido, pregunta, contraseña } = req.body;
 
     try {
-        const query = `
-            INSERT INTO Usuario (usuario, nombre, apellido, pregunta, contraseña)
-            VALUES ($1, $2, $3, $4, $5)
+        const queryUsuario = `
+            SELECT * FROM usuario WHERE usuario = $1
         `;
-        await pool.query(query, [usuario, nombre, apellido, pregunta, contraseña]);
-        res.status(201).send('Usuario registrado correctamente');
+        const resultado = await pool.query(queryUsuario, [usuario]);
+
+        if (resultado.rows.length > 0) {
+            res.status(400).send('El nombre de usuario ya existe. Por favor, elige otro.');
+        } else {
+            const queryregistro = `
+                INSERT INTO usuario (usuario, nombre, apellido, pregunta, contraseña)
+                VALUES ($1, $2, $3, $4, $5)
+            `;
+            const result = await pool.query(queryregistro, [usuario, nombre, apellido, pregunta, contraseña]);
+            res.status(201).send('Usuario registrado correctamente');
+        }
     } catch (error) {
-        console.error('Error al insertar datos:', error);
-        res.status(500).send('Error al registrar el usuario');
+        console.error('Error al insertar datos:', error.message);
+        res.status(500).send(`Error al registrar el usuario: ${error.message}`);
     }
 });
-
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
