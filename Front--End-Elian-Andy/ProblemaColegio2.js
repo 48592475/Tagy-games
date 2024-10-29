63700482bfd78a218784fba4cb0d547ef38c9083
Andy07cama <47961283@est.ort.edu.ar>
2024-10-29 14:19:14 -0300

    Colegio 1 y 2 completas imagenes Colegio 3 y 4 pasadas

---
+++ Front--End-Elian-Andy/ProblemaColegio2.js
@@ -0,0 +1,60 @@
+/*const button = document.createElement("button");
+button.type = "button";
+button.innerText = "button";
+document.body.appendChild(button);*/
+
+document.getElementById('botonTextoProblema1').addEventListener('click', function() {
+    toggleTextBox('textBox1');
+});
+
+document.getElementById('botonLecturaProblema1').addEventListener('click', function() {
+    toggleTextBox('textBox2');
+});
+
+document.getElementById('botonLecturaSolucion1').addEventListener('click', function() {
+    toggleTextBox('textBox3');
+});
+
+document.getElementById('botonTextoSolucion1').addEventListener('click', function() {
+    toggleTextBox('textBox4');
+});
+
+document.getElementById('botonLecturaSolucion2').addEventListener('click', function() {
+    toggleTextBox('textBox5');
+});
+
+document.getElementById('botonTextoSolucion2').addEventListener('click', function() {
+    toggleTextBox('textBox6');
+});
+
+document.getElementById('botonLecturaSolucion3').addEventListener('click', function() {
+    toggleTextBox('textBox7');
+});
+
+document.getElementById('botonTextoSolucion3').addEventListener('click', function() {
+    toggleTextBox('textBox8');
+});
+
+document.getElementById('botontextoSolucion1calle1').addEventListener('click', function() {
+    toggleTextBox('textBox9');
+});
+
+document.getElementById('botonlecturaSolucion1calle1').addEventListener('click', function() {
+    toggleTextBox('textBox10');
+});
+
+function toggleTextBox(id) {
+    var textBox = document.getElementById(id);
+    if (textBox.classList.contains('expanded')) {
+        textBox.classList.remove('expanded');
+        setTimeout(function() {
+            textBox.style.display = 'none';
+        }, 300); 
+    } else {
+        textBox.style.display = 'block';
+        setTimeout(function() {
+            textBox.classList.add('expanded');
+        }, 10); 
+    }
+}
+
