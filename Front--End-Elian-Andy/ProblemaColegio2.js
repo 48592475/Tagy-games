bf4ad4eee221b64481a38888ba6d0877a6b137b5
TobiasSendyk <144272436+48592475@users.noreply.github.com>
2024-11-12 15:35:49 -0300

    Canciones en todas las situaciones

--- Front--End-Elian-Andy/ProblemaColegio2.js
+++ Front--End-Elian-Andy/ProblemaColegio2.js
@@ -3,6 +3,65 @@ button.type = "button";
 button.innerText = "button";
 document.body.appendChild(button);*/
 
+function toggleTextBox(id) {
+    var textBox = document.getElementById(id);
+    if (textBox.classList.contains('expanded')) {
+        textBox.classList.remove('expanded');
+        setTimeout(function() {
+            textBox.style.display = 'none';
+        }, 300); 
+    } else {
+        textBox.style.display = 'block';
+        setTimeout(function() {
+            textBox.classList.add('expanded');
+        }, 10); 
+    }
+}
+
+const videoElement = document.getElementById('videoPlayer');
+videoElement.volume = 0.5;
+
+document.addEventListener('DOMContentLoaded', function () {
+    const consentimientoContainer = document.getElementById('consentimientoContainer');
+    const botonConsentimiento = document.getElementById('botonConsentimiento');
+    const videoElement = document.getElementById('videoPlayer');
+    const videoSource = document.getElementById('videoSource');
+
+    botonConsentimiento.addEventListener('click', async function () {
+        try {
+            // Oculta el botón de consentimiento
+            consentimientoContainer.style.display = 'none';
+            videoElement.style.display = 'block'; // Muestra el video
+
+            // Llama a la API para obtener la URL del video
+            const query = `playRelajante=${false}&playAlegre=${true}`;
+            const response = await fetch(`http://localhost:3000/cancion?${query}`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            const data = await response.json();
+            console.log('Datos recibidos:', data);
+
+            if (response.ok && data.cancion) {
+                const videoUrl = data.cancion.alegres || data.cancion.relajantes;
+                console.log('URL seleccionada:', videoUrl);
+
+                if (videoUrl) {
+                    videoSource.src = videoUrl;
+                    videoElement.load(); // Cargar el video
+                    await videoElement.play().catch(error => console.error('Error en reproducción:', error));
+                } else {
+                    console.error('URL del video no disponible');
+                }
+            } else {
+                console.error('Error en la API:', response.status, data.message);
+            }
+        } catch (error) {
+            console.error('Error al llamar a la API:', error);
+        }
+    });
+});
 document.getElementById('botonTextoProblema1').addEventListener('click', function() {
     toggleTextBox('textBox1');
 });
